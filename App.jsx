d2958a8be1141da2e4fb5ab16272aadcd61429c9
deslrey <2135232170@qq.com>
2024-11-29 10:21:05 +0800

    refactor: 将App修改为js文件

---
+++ App.jsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App组件</Text>
+    </View>
+  )
+}
+
+
+
+export default App;
