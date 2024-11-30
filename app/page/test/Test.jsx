a9a43f5387ecab58bd39e0c1a822fd28f75efdb5
deslrey <2135232170@qq.com>
2024-11-30 23:24:08 +0800

    test: 上传UI测试

--- app/page/test/Test.jsx
+++ app/page/test/Test.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 import { Text, View } from 'react-native';
 
-import AntDesign from "react-native-vector-icons/AntDesign";
 
-import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome6';
 
 export default function Test() {
     return (
         <View>
-            <FontAwesome6 name="rocket" size={30} color="#900" />
+            <FontAwesomeIcon name="chess-queen" size={30} color="#900" />
             <Text>
-                Lorem <FontAwesome6 name="book" color="#4F8EF7" /> Ipsum
+                Lorem <FontAwesomeIcon name="book" color="#4F8EF7" /> Ipsum
             </Text>
         </View>
     )
