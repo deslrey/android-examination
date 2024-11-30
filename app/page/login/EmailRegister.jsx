7d5c03dc86ab92523a0ac42570ff85617328b4d0
deslrey <2135232170@qq.com>
2024-11-30 18:06:52 +0800

    feat: 添加邮箱和手机号注册

---
+++ app/page/login/EmailRegister.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const EmailRegiste = () => {
+  return (
+    <View>
+      <Text style={{ backgroundColor: '#8fd3f4' }}>我是EmailRegiste</Text>
+    </View>
+  )
+}
+
+
+
+export default EmailRegiste;
---
+++ app/page/login/PhoneRegister.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PhoneRegister = () => {
+    return (
+        <View>
+            <Text style={{ backgroundColor: '#9890e3' }}>我是PhoneRegister</Text>
+        </View>
+    )
+}
+
+export default PhoneRegister;
