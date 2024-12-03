bf63f5945f3d3d054aca870173c1da3cb4a6e00b
deslrey <2135232170@qq.com>
2024-12-03 14:51:03 +0800

    feat: 添加用户头像及头像点击事件

--- app/page/mine/MinePage.jsx
+++ app/page/mine/MinePage.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { View } from 'react-native';
-import { Text } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+import { Avatar } from '@rneui/themed';
+
 
 const MinePage = () => {
-    return (
-        <View>
-            <Text>MinePage</Text>
-        </View>
-    )
-}
 
+    const avatarClick = () => {
+        console.log('头像被点击了');
+    }
+
+    return (
+        <>
+            <ScrollView>
+                <View
+                    style={styles.avatarStyle}
+                >
+                    <Avatar
+                        size={64}
+                        rounded
+                        title="Fc"
+                        containerStyle={{ backgroundColor: '#8fd3f4', }}
+                        onPress={avatarClick}
+                    />
+                </View>
+            </ScrollView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    avatarStyle: {
+        paddingTop: '1%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 30,
+    }
+})
 
 export default MinePage;
