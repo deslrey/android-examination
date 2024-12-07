5bf2aea41758957405609145bc55cca4948141a1
deslrey <2135232170@qq.com>
2024-12-07 16:14:58 +0800

    feat: 创建搜索组件

---
+++ app/components/home/SearchComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+const SearchComponent = () => {
+    return (
+        <View>
+            <Text h2>我是SearchComponent</Text>
+        </View>
+    )
+}
+
+export default SearchComponent;
