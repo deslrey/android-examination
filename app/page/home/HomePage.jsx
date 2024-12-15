b4b52ef85c0f71dd592d29cc3ab2c2812039a6a9
deslrey <2135232170@qq.com>
2024-12-15 17:36:35 +0800

    feat: 主页设置背景图片

--- app/page/home/HomePage.jsx
+++ app/page/home/HomePage.jsx
@@ -1,12 +1,30 @@
-import { Text } from '@rneui/themed';
-import React from 'react'
+import React from 'react';
+import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-elements';
 
-const HomeDeslre = () => {
+const image = require('../../static/images/lbl.png');
+
+const HomePage = () => {
     return (
-        <>
-            <Text h1>Home界面</Text>
-        </>
-    )
-}
+        <View style={styles.container}>
+            <ImageBackground source={image} style={styles.image}>
 
-export default HomeDeslre;
+            </ImageBackground>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+});
+
+export default HomePage;
