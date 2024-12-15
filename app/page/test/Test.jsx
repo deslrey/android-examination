9cdb921fb68aece339018dc032feb4ac07c403a2
deslrey <2135232170@qq.com>
2024-12-15 17:31:49 +0800

    test: 上传测试背景图片

--- app/page/test/Test.jsx
+++ app/page/test/Test.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-import { Text, View } from 'react-native';
+// 使用 require 加载本地图片
+const image = require('../../static/images/lbl.png'); // 根据当前文件相对路径
 
 
-import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome6';
+const App = () => (
+    <View style={styles.container}>
+        <ImageBackground source={image} style={styles.image}>
+            <Text style={styles.text}>Inside</Text>
+        </ImageBackground>
+    </View>
+);
 
-export default function Test() {
-    return (
-        <View>
-            <FontAwesomeIcon name="chess-queen" size={30} color="#900" />
-            <Text>
-                Lorem <FontAwesomeIcon name="book" color="#4F8EF7" /> Ipsum
-            </Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    text: {
+        color: "white",
+        fontSize: 42,
+        fontWeight: "bold",
+        textAlign: "center",
+        backgroundColor: "#000000a0" // 使用 backgroundColor 替换 background
+    }
+});
+
+export default App;
