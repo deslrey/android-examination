e4253db543cad3f5cf7d4b8b46cd4e1f7acfdc11
deslrey <2135232170@qq.com>
2024-12-16 21:07:53 +0800

    feat: 上传复习组件

---
+++ app/components/ReviewComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+const ReviewComponent = () => {
+    return (
+        <View>
+            <Text h1>ReviewComponent</Text>
+        </View>
+    )
+}
+
+export default ReviewComponent;
