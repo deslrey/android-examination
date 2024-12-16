6a7a8319ab6224af5e4ab6874472af605057cbaa
deslrey <2135232170@qq.com>
2024-12-16 19:32:27 +0800

    feat: 上传学习单词组件

---
+++ app/components/LearnComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native';
+import { Text } from 'react-native-elements';
+
+const LearnComponent = () => {
+    return (
+        <View>
+            <Text h1>LearnComponent</Text>
+        </View>
+    )
+}
+
+export default LearnComponent;
