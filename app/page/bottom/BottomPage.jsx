602b0dea21aedfcc8d727c3ca8e3ea7742e52ccb
deslrey <2135232170@qq.com>
2024-12-17 10:17:56 +0800

    feat: 上传主界面底部page

---
+++ app/page/bottom/BottomPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const BottomPage = () => {
+    return (
+        <View>BottomPage</View>
+    )
+}
+
+
+
+export default BottomPage;
