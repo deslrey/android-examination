cf85d15951301ddc913d6e5eb14ccca51b199bf4
deslrey <2135232170@qq.com>
2024-12-17 10:18:34 +0800

    feat: 修改从useNavigation()中获取navigation

--- app/page/home/HomePage.jsx
+++ app/page/home/HomePage.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { View, StyleSheet, ImageBackground } from 'react-native';
 import { MessageProvider } from '../../utils/Message';
+import { useNavigation } from '@react-navigation/native';
 import BoxComponent from '../../components/boxs/BoxComponent';
 import SignInButton from '../../components/sign/SignInButton';
 import useHomePageLogic from '../../hooks/useHomePageLogic';
 
 const image = require('../../static/images/lbl.png');
 
-const HomePage = ({ navigation }) => {
-    const { hasSignedIn, handleSignIn, toLearn, toReview } = useHomePageLogic(navigation); // 使用自定义 hook
+const HomePage = () => {
+
+    const navigation = useNavigation();
+    const { hasSignedIn, handleSignIn, toLearn, toReview } = useHomePageLogic(navigation);
+
 
     return (
         <MessageProvider>
             <View style={styles.container}>
                 <ImageBackground source={image} style={styles.image}>
-                    {/* 签到按钮 */}
                     <SignInButton hasSignedIn={hasSignedIn} onSignIn={handleSignIn} />
-
-                    {/* 两个盒子 */}
                     <View style={styles.boxContainer}>
                         <BoxComponent title="Learn" onPress={toLearn} />
                         <BoxComponent title="Review" onPress={toReview} />
