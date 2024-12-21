95604c6cb849695fca6cec030fd9be9d37fd5a4d
deslrey <2135232170@qq.com>
2024-12-21 09:54:42 +0800

    feat: codePage界面添加Tab组件进行选择

--- app/page/code/CodePage.jsx
+++ app/page/code/CodePage.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-elements'
+import React from 'react';
+import { Tab, Text, TabView } from '@rneui/themed';
+import { StyleSheet, View } from 'react-native';
 
-export default function CodePage() {
+const CodePage = () => {
+    const [index, setIndex] = React.useState(0);
+
     return (
-        <View>
-            <Text h1>CodePage</Text>
-        </View>
-    )
-}
+        <>
+            <View>
+                <Text>代码Api界面</Text>
+            </View>
+            <Tab
+                value={index}
+                onChange={(e) => setIndex(e)}
+                indicatorStyle={{
+                    backgroundColor: 'white',
+                    height: 3,
+                }}
+                variant="primary"
+            >
+                <Tab.Item
+                    title="Recent"
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'timer', type: 'ionicon', color: 'white' }}
+                />
+                <Tab.Item
+                    title="favorite"
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'heart', type: 'ionicon', color: 'white' }}
+                />
+                <Tab.Item
+                    title="cart"
+                    titleStyle={{ fontSize: 12 }}
+                    icon={{ name: 'cart', type: 'ionicon', color: 'white' }}
+                />
+            </Tab>
+
+            <TabView value={index} onChange={setIndex} animationType="spring">
+                <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
+                    <Text h1>Recent</Text>
+                </TabView.Item>
+                <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
+                    <Text h1>Favorite</Text>
+                </TabView.Item>
+                <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
+                    <Text h1>Cart</Text>
+                </TabView.Item>
+            </TabView>
+        </>
+    );
+};
+
+
+const styles = StyleSheet.create({
+
+})
+
+
+export default CodePage;
