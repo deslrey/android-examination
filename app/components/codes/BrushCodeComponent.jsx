df94ebf2df03572f920532649fd099e53cf04c95
deslrey <2135232170@qq.com>
2024-12-25 17:11:48 +0800

    feat: 添加刷单词组件

---
+++ app/components/codes/BrushCodeComponent.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BrushCodeComponent = () => {
+    return (
+        <View>
+            <Text>BrushCodeComponent</Text>
+        </View>
+    )
+}
+
+export default BrushCodeComponent
