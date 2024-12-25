296c6750336823820d575c28e0bcc8e5ef01442f
deslrey <2135232170@qq.com>
2024-12-25 10:43:45 +0800

    style: code界面测试编程语言的图标使用

--- app/page/code/CodePage.jsx
+++ app/page/code/CodePage.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Tab, Text, TabView } from '@rneui/themed';
-import { StyleSheet, View } from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome6';  // 使用FontAwesome6图标库
 
 const CodePage = () => {
     const [index, setIndex] = React.useState(0);
 
     return (
         <>
-            <View>
-                <Text>代码Api界面</Text>
-            </View>
             <Tab
                 value={index}
                 onChange={(e) => setIndex(e)}
@@ -37,24 +34,19 @@ const CodePage = () => {
             </Tab>
 
             <TabView value={index} onChange={setIndex} animationType="spring">
-                <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
-                    <Text h1>Recent</Text>
+                <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
+                    <Text h1>Recent   <FontAwesomeIcon name="java" size={30} color="red" /></Text>
+
                 </TabView.Item>
-                <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
-                    <Text h1>Favorite</Text>
+                <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
+                    <Text h1>Favorite  <FontAwesomeIcon name="neos" size={30} color="red" /></Text>
                 </TabView.Item>
-                <TabView.Item style={{ backgroundColor: 'green', width: '100%' }}>
+                <TabView.Item style={{ backgroundColor: 'white', width: '100%' }}>
                     <Text h1>Cart</Text>
                 </TabView.Item>
             </TabView>
         </>
     );
 };
-
-
-const styles = StyleSheet.create({
-
-})
-
 
 export default CodePage;
