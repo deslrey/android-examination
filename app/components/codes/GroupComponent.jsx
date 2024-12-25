4c561f9f7a06b20509f00fd170cfb80d5ef9d01a
deslrey <2135232170@qq.com>
2024-12-25 17:13:39 +0800

    feat: 添加单词分组组件

---
+++ app/components/codes/GroupComponent.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const GroupComponent = () => {
+    return (
+        <View>
+            <Text>GroupComponent</Text>
+        </View>
+    )
+}
+
+export default GroupComponent
+
+const styles = StyleSheet.create({})
