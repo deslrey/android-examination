a405f5a1560ab9daf60af92669d4b4e8c3eddfa9
deslrey <2135232170@qq.com>
2024-12-27 09:41:57 +0800

    feat: 主页实现从后端获取一个单词进行展示

--- app/components/sign/SignInButton.jsx
+++ app/components/sign/SignInButton.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
+
+const PrefixApi = 'http://192.168.31.10:808/deslre';
 
 const SignInButton = ({ hasSignedIn, onSignIn }) => {
     // 获取当前日期并格式化为 "YYYY-MM-DD" 格式
     const currentDate = new Date().toLocaleDateString('zh-CN');
+    const [word, setWord] = useState('deslre');
+
+
+    const getRandomWord = async () => {
+        try {
+            // const response = await axios.get(`${PrefixApi}/words/getRandom`)
+            console.log('result ======> ', response.data);
+            setWord(response.data.message)
+
+        } catch (error) {
+        }
+    }
+
+    getRandomWord()
+
+    useEffect(() => {
+        const getRandomWord = async () => {
+            try {
+                const response = await axios.get(`${PrefixApi}/words/getRandom`)
+                console.log('result ======> ', response.data);
+                setWord(response.data.message)
+
+            } catch (error) {
+                setWord('desslre')
+            }
+        }
+        getRandomWord()
+    }, []);  // 组件加载时只执行一次
 
     return (
         <View style={styles.signInContainer}>
@@ -11,8 +42,8 @@ const SignInButton = ({ hasSignedIn, onSignIn }) => {
                 <Text style={styles.signedInText}>今日已签到</Text>
             ) : (
                 <View style={styles.signInButtonContainer}>
-                    <TouchableOpacity style={styles.signInButton} onPress={onSignIn}>
-                        <Text style={styles.signInText}>签到</Text>
+                    <TouchableOpacity style={styles.signInButton}>
+                        <Text style={styles.signInText}>{word}</Text>
                     </TouchableOpacity>
                     <Text style={styles.dateText}>今日: {currentDate}</Text>
                 </View>
